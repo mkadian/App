f064eed54369206356fe1d7f606e35c33b5d5fff
mkadian <mitsmessi133@gmail.com>
2021-04-29 07:51:55 +0530

    Todo App

---
+++ tempData.js
@@ -0,0 +1,70 @@
+// Temporary Data until we connect with Firebase
+
+export default tempData = [
+    {
+        name: "Plan a trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passpart Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: false 
+            },
+            {
+                title: "Pack Luggage",
+                completed: false 
+            }
+        ]
+    },
+    {
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passpart Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true 
+            },
+            {
+                title: "Pack Luggage",
+                completed: true 
+            },
+            {
+                title: "Take dog for a walk",
+                completed: true
+            }
+        ]
+    },
+    {
+        name: "Birthday Party",
+        color: "#595BD9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passpart Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true 
+            }
+        ]
+    },
+];
